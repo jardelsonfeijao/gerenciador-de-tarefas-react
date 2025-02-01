f80b56559089000c319836268cc1f17f072bce98
jardelsonfeijao <jardelsonfeijao04@gmail.com>
2025-01-31 23:38:32 -0300

    style: aplica estilos ao App.jsx e importa Tailwind (WIP)

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,9 @@
 function Tasks(props) {
   console.log(props);
   return (
-    <ul className="">
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
-        <li key={task.id} className="">
+        <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">
           {task.title}
         </li>
       ))}
