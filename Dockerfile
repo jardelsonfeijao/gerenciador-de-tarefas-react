9c8203cc51489d7843af6bc43b6df2a5e46084f0
jardelsonfeijao <jardelsonfeijao04@gmail.com>
2025-03-06 22:32:41 -0300

    chore: adiciona Dockerfile e docker-compose.yml para dockerização da aplicação

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
 FROM node
+
+WORKDIR /app
+
+COPY package*.json /app/
+
+RUN npm install
+
+COPY . /app/
+
+RUN npm run build
+
+EXPOSE 4173
+
+CMD ["npm", "run", "preview", "--", "--host"]
--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,22 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  useEffect(() => {
+    const fetchTasks = async () => {
+      // CHAMAR A API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      // PEGAR OS DADOS QUE ELA RETORNA
+      // ARMAZENAR / PERSISTIR ESSES DADOS NO STATE
+    };
+    fetchTasks();
+  }, []);
+
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       // CASO PRECISE ATUALIZAR A TAREFA
